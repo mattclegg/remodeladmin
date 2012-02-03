39935e3a8a3dc5c47c10c4c6a466451b7c0de929
Juerg Rast <juergr@gmail.com>
2012-02-03 22:20:28 +0100

    Added language support for de_DE

---
+++ lang/en_US.php
@@ -0,0 +1,20 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['DateRange']['END'] = 'end';
+$lang['en_US']['DateRange']['START'] = 'start';
+$lang['en_US']['ModelAdmin']['DELETED'] = 'Deleted';
+$lang['en_US']['ModelAdmin']['DELETEDFROMLIVE'] = 'Deleted';
+$lang['en_US']['ModelAdmin']['PUBLISHED'] = 'Published';
+$lang['en_US']['ModelAdmin']['ROLLEDBACK'] = 'Rolled back version';
+$lang['en_US']['ModelAdmin']['SAVED'] = 'Saved';
+$lang['en_US']['ModelAdmin']['UNPUBLISHED'] = 'Unpublished';
+$lang['en_US']['RemodelAdmin']['BACKTOLIST'] = 'Back to list view';
+$lang['en_US']['RemodelAdmin']['LOADING'] = 'Loading...';
+$lang['en_US']['RemodelAdmin']['PARENTPAGE'] = 'Parent page';
+$lang['en_US']['RemodelAdmin']['VIEW'] = 'View Page';
+$lang['en_US']['RemodelAdmin']['VIEWDRAFT'] = 'Draft Site';
+$lang['en_US']['RemodelAdmin']['VIEWLIVE'] = 'Live Site';
+
+?>
